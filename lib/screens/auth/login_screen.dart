e992962613d2ac35590b3ea1f93b7557be2d2fc7
Taha Hamada <taha2901>
2024-05-20 04:29:38 +0300

    refactor text field  and use it

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,7 +1,9 @@
 import 'package:chat/utils/colors.dart';
 import 'package:chat/widgets/logo.dart';
+import 'package:chat/widgets/text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -13,6 +15,8 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
+    TextEditingController emailCon = TextEditingController();
+    TextEditingController passCon = TextEditingController();
     return Scaffold(
       appBar: AppBar(),
       body: SingleChildScrollView(
@@ -29,6 +33,24 @@ class _LoginScreenState extends State<LoginScreen> {
             Text(
               'New Material Chat App With Taha',
               style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            Column(
+              children: [
+                CustomField(
+                      controller: emailCon,
+                      lable: "Email",
+                      icon: Iconsax.direct,
+                    ),
+                    CustomField(
+                      controller: passCon,
+                      lable: "Password",
+                      icon: Iconsax.password_check,
+                      isPass: true,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+              ],
             ),
           ],
         ),

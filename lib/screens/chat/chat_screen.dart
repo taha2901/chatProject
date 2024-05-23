250a42b7ffa96676c3018eefb81b31518d33746a
Taha Hamada <taha2901>
2024-05-23 18:09:47 +0300

    appBar in chat page is done

--- lib/screens/chat/chat_screen.dart
+++ lib/screens/chat/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -11,8 +12,28 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      
+      appBar: AppBar(
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("Nabil"),
+            Text(
+              "Last Seen 11:28 am",
+              style: Theme.of(context).textTheme.labelLarge,
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Iconsax.trash),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Iconsax.copy),
+          ),
+        ],
+      ),
     );
   }
 }
